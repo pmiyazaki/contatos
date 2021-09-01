27d28ee4cc98f1aa23db0bdb4f20e5ab2d0440ef
Patricia <pmiyazakii@gmail.com>
2021-09-01 16:36:37 -0300

    Adicionado novo Contato no CreateContato

--- src/app/components/create-contato/create-contato.component.ts
+++ src/app/components/create-contato/create-contato.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Contato } from 'src/app/models/Contato';
 
 @Component({
   selector: 'app-create-contato',
@@ -6,14 +7,24 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styleUrls: ['./create-contato.component.css']
 })
 export class CreateContatoComponent implements OnInit {
+   
+  novoContato:Contato = {
+    nome:'',
+    email:'',
+    telefones:['']
+  }
 
   @Output() onCancelarClick:EventEmitter<null> = new EventEmitter();
 
   constructor() { }
 
-  cancelar() {
+  cancelar():void {
     console.log('Pediu para cancelar');
     this.onCancelarClick.emit()
+  }
+
+  track(index:number, value:string){
+    return index;
   }
 
   ngOnInit(): void {
