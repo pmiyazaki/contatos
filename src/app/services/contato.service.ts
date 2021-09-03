73737a9a28974c3bcad54e5751dfab3b199ba7a2
Patricia <pmiyazakii@gmail.com>
2021-09-03 14:26:10 -0300

    Dando pra adicionar novos contatos

--- src/app/services/contato.service.ts
+++ src/app/services/contato.service.ts
@@ -59,15 +59,37 @@ const BASE_DE_CONTATOS:Contato[] = [
 })
 export class ContatoService {
 
-  constructor() { }
+  private baseDeContatos:Contato[];
+  private chave:string = 'CONTATOS';
+
+  constructor() {
+    
+    //Carregando informações do localStorage na chave CONTATOS
+    let dados = window.localStorage.getItem(this.chave)
+    
+    //Verificando se as informações existem
+    if(dados){
+      //Existe: transformando dados em array e guardando em baseDeContatos
+      this.baseDeContatos = JSON.parse(dados);
+    
+    } else {
+      
+      //Não existe: Pões uma string json com array vazio no localStorage
+      window.localStorage.setItem(this.chave, '[]');
+      
+      //Colocando um array vazio no atributo baseDeContatos
+      this.baseDeContatos = [];
+    }
+  }
 
   getContatos():Contato[] {
-    return BASE_DE_CONTATOS;
+    return this.baseDeContatos;
   }
 
   //receber um contato, adicionar o contato BASE_DE_CONTATOS e retornar nada.
   addContato(c:Contato):void {
-    BASE_DE_CONTATOS.push(c);
+    this.baseDeContatos.push(c);
+    window.localStorage.setItem(this.chave, JSON.stringify(this.baseDeContatos));
   }
 
 }
